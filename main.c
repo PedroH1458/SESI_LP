936df30dda0c60d25ac1433c997811f8e9db7aa5
Pedro <pedrohenriquecolorado10@gmail.com>
2025-07-25 11:17:46 -0300

    Meu primeiro  repositório

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    
+    printf("Ola, bom dia \n");
+    
+    int A;
+    int B;
+    A=1;
+    B=25;
+    
+    printf("%d %d", A, B );
+    
+    
+
+    
+}
