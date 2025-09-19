63334388a10f57f3a699a8b8544f96ab18c645a4
PedroH1458 <pedrohenriquecolorado10@gmail.com>
2025-09-19 09:27:52 -0300

    Update vetor de 5 inteiros.c.c

--- vetor de 5 inteiros.c.c
+++ vetor de 5 inteiros.c.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int v[10], i, s, m, M;
+    int vetor[10], i, s, m, M;
 
-    for(i=0; i<5; i++) scanf("%d", &v[i]);
-    for(i=0; i<5; i++) printf("%d ", v[i]);
+    for(i=0; i<5; i++) scanf("%d", &vetor[i]);
+    for(i=0; i<5; i++) printf("%d ", vetor[i]);
     printf("\n");
 
     s = 0;
-    for(i=0; i<10; i++) { scanf("%d", &v[i]); s += v[i]; }
+    for(i=0; i<10; i++) { scanf("%d", &vetor[i]); s += vetor[i]; }
     printf("Soma: %d\n", s);
 
-    for(i=0; i<8; i++) scanf("%d", &v[i]);
-    M = m = v[0];
+    for(i=0; i<8; i++) scanf("%d", &vetor[i]);
+    M = m = vetor[0];
     for(i=1; i<8; i++) {
-        if(v[i] > M) M = v[i];
-        if(v[i] < m) m = v[i];
+        if(vetor[i] > M) M = vetor[i];
+        if(vetor[i] < m) m = vetor[i];
     }
     printf("Maior: %d | Menor: %d\n", M, m);
 
     s = 0;
-    for(i=0; i<6; i++) { scanf("%d", &v[i]); s += v[i]; }
+    for(i=0; i<6; i++) { scanf("%d", &vetor[i]); s += vetor[i]; }
     printf("Media: %.2f\n", s/6.0);
 
     return 0;
